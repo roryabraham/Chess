c088eb4dbf900aa56919e1660103b456cccc046d
KashmirMonkey <rabraham@sandiego.edu>
2019-07-02 16:04:49 -0700

    path validation nearing completion

--- index.js
+++ index.js
@@ -209,6 +209,109 @@ function locationIsValid(location) {
     }
 }
 
+function pathIsOpen(src, dest) {
+    let srcCol = src.charAt(0);
+    let srcRow = src.charAt(1);
+    let destCol = dest.charAt(0);
+    let destRow = dest.charAt(1);
+    let currCol = srcCol;
+    let currRow = srcRow;
+
+    // If this is a diagonal path
+    if(srcCol !== destCol && srcRow !== destRow) {
+        // Determine direction to traverse
+        if(srcCol < destCol) // left-to-right
+        {
+            if(srcRow < destRow) // top-to-bottom
+            {
+                currCol = nextCol[currCol];
+                for(currRow = --currRow; currRow > destRow; currRow--) {
+                    if($("#"+currCol+currRow).hasClass("containsPiece")) {
+                        return false;
+                    }
+                    currCol = nextCol[currCol];
+                }
+            }
+            else // bottom-to-top
+            {
+                currCol = nextCol[currCol];
+                for(currRow = ++currRow; currRow < destRow; currRow++) {
+                    if($("#"+currCol+currRow).hasClass("containsPiece")) {
+                        return false;
+                    }
+                    currCol = nextCol[currCol];
+                }
+            }
+        }
+        else // right-to-left
+        {
+            if(srcRow < destRow) // top-to-bottom
+            {
+                currCol = prevCol[currCol];
+                for(currRow = --currRow; currRow > destRow; currRow--) {
+                    if($("#"+currCol+currRow).hasClass("containsPiece")) {
+                        return false;
+                    }
+                    currCol = prevCol[currCol];
+                }
+            }
+            else // bottom-to-top
+            {
+                currCol = prevCol[currCol];
+                for(currRow = ++currRow; currRow < destRow; currRow++) {
+                    if($("#"+currCol+currRow).hasClass("containsPiece")) {
+                        return false;
+                    }
+                    currCol = prevCol[currCol];
+                }
+            }
+        }
+    }
+
+    // if this is a vertical path
+    else if(srcCol === destCol) {
+        // determine the direction to traverse
+        if(srcRow < destRow) // bottom-to-top
+        {
+            for(currRow = ++currRow; currRow < destRow; currRow++) {
+                if($("#"+srcCol+currRow).hasClass("containsPiece")) {
+                    return false;
+                }
+            }
+        }
+        else // top-to-bottom
+        {
+            for(currRow = --currRow; currRow > destRow; currRow--) {
+                if($("#"+srcCol+currRow).hasClass("containsPiece")) {
+                    return false;
+                }
+            }
+        }
+    }
+    else // this is a horizontal path
+    {
+        // determine the direction to traverse
+        if(srcCol < destCol) // left-to-right
+        {
+            for(currCol = nextCol[currCol]; currCol < destCol; currCol = nextCol[currCol]) {
+                if($("#"+currCol+srcRow).hasClass("containsPiece")) {
+                    return false;
+                }
+            }
+        }
+        else // right-to-left
+        {
+            for(currCol = prevCol[currCol]; currCol > destCol; currCol = prevCol[currCol]) {
+                if($("#"+currCol+srcRow).hasClass("containsPiece")) {
+                    return false;
+                }
+            }
+        }
+    }
+
+    return true;
+}
+
 function evaluateValidMoves(location, pieceType, potentialMoves) {
     // assume all moves are valid
     let validMoves = potentialMoves;
@@ -255,7 +358,7 @@ function evaluateValidMoves(location, pieceType, potentialMoves) {
             let destCol = location.charAt(1);
             if(srcCol !== destCol) {
                 // if destination is empty, invalidate diagonal move
-                if(destElem.html() === "") {
+                if(!destElem.hasClass("containsPiece")) {
                     validMoves.remove(potentialMoves[i]);
                     continue;
                 }
@@ -263,7 +366,7 @@ function evaluateValidMoves(location, pieceType, potentialMoves) {
             // Pawns also can't attack forward
             else {
                 // if destination is nonempty, invalidate forward move
-                if(destElem.html() !== "") {
+                if(destElem.hasClass("containsPiece")) {
                     validMoves.remove(potentialMoves[i]);
                     continue;
                 }
@@ -272,7 +375,10 @@ function evaluateValidMoves(location, pieceType, potentialMoves) {
 
         // If there is a piece blocking the path, invalidate move (except knights can jump)
         if(pieceType !== "knight") {
-
+            if(!pathIsOpen(potentialMoves[i])) {
+                validMoves.remove(potentialMoves[i]);
+                continue;
+            }
         }
     }
 }
